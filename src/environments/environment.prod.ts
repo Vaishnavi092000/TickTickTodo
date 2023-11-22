3c8e94f2a2d21818fbb006d7cf67a4c4d2d353c7
Vaishnavi Gulwelkar <127302593+Vaishnavi092000@users.noreply.github.com>
2023-11-22 10:11:45 +0530

    Update environment.prod.ts
    for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Your web app's Firebase configuration
+  firebase : {
+    apiKey: "AIzaSyB-vBxAjkLlHlyB85Y0mS1xJsmdhoVNB74",
+    authDomain: "tick-tick-a2256.firebaseapp.com",
+    projectId: "tick-tick-a2256",
+    storageBucket: "tick-tick-a2256.appspot.com",
+    messagingSenderId: "725922862747",
+    appId: "1:725922862747:web:068ebc65e8916fad72183e"
+  }
 };
